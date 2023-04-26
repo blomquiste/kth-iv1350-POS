1eafb325a11282adcf1caa3fbdac9f4e1052212c
IconRob <100430718+IconRob@users.noreply.github.com>
2023-04-26 17:55:03 +0200

    added prompt (#11)

--- src/se/kth/iv1350/view/View.java
+++ src/se/kth/iv1350/view/View.java
@@ -3,9 +3,13 @@ package src.se.kth.iv1350.view;
 import src.se.kth.iv1350.controller.Controller;
 import src.se.kth.iv1350.dto.CurrentSaleDTO;
 
+import java.util.Scanner;
+
 public class View {
     private CurrentSaleDTO saleInfo;
     private Controller contr;
+
+    Scanner input = new Scanner(System.in);
 
     // 1. En med prompt
     // 2. Fil med itemID och ev. quantity
@@ -13,12 +17,39 @@ public class View {
 
 // 3.
     public void hardkodadegrejer() {
+        System.out.println("Welcome Veggie Shop \n");
         contr.startSale();
-        System.out.println(contr.registerItem(5));
-        System.out.println(contr.registerItem(5));
-        System.out.println(contr.registerItem(8, 2));
-        System.out.println(contr.registerItem(1));
-        System.out.println(contr.registerItem(1, 2));
+        System.out.println("New buy: \n Add items to the shoppingcart by typing (ID Quantity)\n");
+        System.out.println(contr.registerItem(input.nextInt(),input.nextInt()));
+
+
+        String answer = "y";
+        while (answer.equalsIgnoreCase("y")) {
+            System.out.println("\nDo you want to add more items (y/n) ");
+            answer = input.next();
+
+            if (answer.equalsIgnoreCase("y")) {
+                System.out.println("Enter next item (ID Quantity)\n");
+                System.out.println((contr.registerItem(input.nextInt(),input.nextInt()) + "\n"));
+
+            } else if (answer.equalsIgnoreCase("n")) {
+                System.out.println("The purchase is completed...");
+                System.out.println("Enter personalID for discount (YYMMDDXXXX)");
+                //Går och kontrollerar ifall ID är korrekt för discounut och ändrar totalbeloppet.
+
+            } else {
+                System.out.println("error message:.. WRONG INPUT");
+                answer = "y";
+
+            }
+        }
+
+//        System.out.println(contr.registerItem(5));
+//        System.out.println(contr.registerItem(5));
+//        System.out.println(contr.registerItem(8, 2));
+//        System.out.println(contr.registerItem(1));
+//        System.out.println(contr.registerItem(1, 2));
+
     }
 
     /**
